a6b318afcfb82e037a78086692976119d8f16df5
Gabriel Massaia de Oliveira <gabrielmassaia50@gmail.com>
2024-04-04 17:35:38 -0300

    mudei pra async e await as functions

--- controller/atualizaCliente-controler.js
+++ controller/atualizaCliente-controler.js
@@ -1,25 +1,24 @@
 import { clienteService } from "../service/cliente-service.js"
 
+(async () => {
 const pegaURL = new URL(window.location)
 
 const id = pegaURL.searchParams.get('id')
 
-
 const inputNome = document.querySelector('[data-nome]')
 const inputEmail = document.querySelector('[data-email]')
 
-clienteService.detalheCliente(id)
-.then(dados => {
+const dados = await clienteService.detalheCliente(id)
     inputNome.value = dados.nome
     inputEmail.value = dados.email
-})
 
 const formulario = document.querySelector('[data-form]')
-formulario.addEventListener('submit', (evento) => {
+
+formulario.addEventListener('submit', async (evento) => {
     evento.preventDefault()
 
-    clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
-    .then(() => {
-        window.location.href = '../telas/edicao_concluida.html'
-    })
+    await clienteService.atualizaCliente(id, inputNome.value, inputEmail.value)
+    window.location.href = '../telas/edicao_concluida.html'  
 })
+}) ()
+
